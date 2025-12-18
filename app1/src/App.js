a3158f8de564c7114ffce0fbc06e81cf5e4730b2
Apirak Kaewpachum <85723194+shidosaiga@users.noreply.github.com>
2025-12-18 11:57:42 +0700

    divStyle

--- app1/src/App.js
+++ app1/src/App.js
@@ -3,13 +3,15 @@ import './style.css';
 import "./App.css";
 
 function App() {
-  return (
-    <>
-    <h2> DeveloperThai.com</h2>
-    <div className="title">React & React Native</div>
-    </>
-  
-  );
+  const divStyle = {
+color: 'red',
+backgroundColor: 'powderblue',
+fontSize: 'larger',
+padding: '3px',
+}
+
+  return <div style={divStyle}>Hello React</div>
+
 }
 
 export default App;
