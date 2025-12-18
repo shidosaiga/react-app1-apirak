e408485c5209ad42bd9b5d445dce9eaec69cf6c1
Apirak Kaewpachum <85723194+shidosaiga@users.noreply.github.com>
2025-12-18 11:49:27 +0700

    add CSS styles with JSX

--- app1/src/App.js
+++ app1/src/App.js
@@ -4,20 +4,11 @@ import "./App.css";
 
 function App() {
   return (
-    
-    <table border="1">
-      <tr><th>Product</th>
-      <th>Price</th></tr>
-      
-      <tr>
-        <td>React</td>
-        <td>320</td>
-      </tr>
-      <tr>
-        <td>React Native</td>
-        <td>340</td>
-      </tr>
-    </table>
+    <>
+    <h2> DeveloperThai.com</h2>
+    <div className="title">React & React Native</div>
+    </>
+  
   );
 }
 
