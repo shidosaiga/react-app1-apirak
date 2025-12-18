a57bdb48e461ddaa4913ccd3833ded748eabfece
Apirak Kaewpachum <85723194+shidosaiga@users.noreply.github.com>
2025-12-18 11:47:27 +0700

    fix table

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
-import h2 from "./style.css"
+import './style.css';
 import "./App.css";
 
 function App() {
   return (
     
     <table border="1">
-      <th>Product</th>
-      <th>Price</th>
+      <tr><th>Product</th>
+      <th>Price</th></tr>
+      
       <tr>
         <td>React</td>
         <td>320</td>
