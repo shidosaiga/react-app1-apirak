95ae04f4e6ddbe98de2aab730de18f0f9af34e89
apirak <netapirak@gmail.com>
2025-12-25 09:38:28 +0700

    add External styling

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.svg";
-import './style.css';
+import "./style.css";
 import "./App.css";
 
 function App() {
-  const divStyle = {
-color: 'red',
-backgroundColor: 'powderblue',
-fontSize: 'larger',
-padding: '3px',
-}
+  // const divStyle = {
+  //   color: "red",
+  //   backgroundColor: "powderblue",
+  //   fontSize: "larger",
+  //   padding: "3px",
+  // };
 
-  return <div style={divStyle}>Hello React</div>
-
+  // return <div style={divStyle}>Hello React</div>;
+  return ( 
+    <>
+    <h2> DeveloperThai.com</h2>
+    <div className="title">React & React Native</div>
+    </>
+  
+  );
 }
 
 export default App;
