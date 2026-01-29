10a0d99add5dc1f5d57b485383e358bd17fc246c
Apirak Kaewpachum <85723194+shidosaiga@users.noreply.github.com>
2026-01-29 10:13:11 +0700

    Context

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,13 +1,16 @@
 // import logo from "./logo.svg";
 // import "./style.css";
 import "./App.css";
-import { Content, Header,Footer} from './func-components'
+// import { Content, Header,Footer} from './func-components'
 // import Button from './class-components'
 import {Calculator2} from './calculator'
 import {EventData2} from './event-data'
 import RefsFunc from './refs-class'
 import RefArray from './refs-array'
 import MessageBox from './state-func'
+import {userContext} from './context'
+import Content  from './context-content'
+
 function App() {
 
   // return (
@@ -19,7 +22,11 @@ function App() {
   // );
   // return <Button/> 
   // return <Calculator2/> 
-  return <MessageBox/>
+  return (
+    <userContext.Provider value = {'apirak'}>
+      <Content/>
+    </userContext.Provider>
+  )
 }
 
 export default App;
